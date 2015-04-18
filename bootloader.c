084a5af75c1c482460a4fa267678751b4bbc94e8
Richard Hughes <richard@hughsie.com>
2015-04-18 10:56:46 +0100

    Do not allow the bootloader to ever write over itself
    I don't know how this can happen, except for perhaps when the address value
    becomes corrupted and wraps-around.

--- bootloader.c
+++ bootloader.c
@@ -199,7 +199,8 @@ ProcessIO(void)
 			(const void *) &RxBuffer[CH_BUFFER_INPUT_DATA+0],
 			2);
 		/* allow to erase any address but not the bootloader */
-		if (address < CH_EEPROM_ADDR_RUNCODE) {
+		if (address < CH_EEPROM_ADDR_RUNCODE ||
+		    address > CH_EEPROM_ADDR_MAX) {
 			rc = CH_ERROR_INVALID_ADDRESS;
 			break;
 		}
@@ -239,7 +240,8 @@ ProcessIO(void)
 		memcpy (&address,
 			(const void *) &RxBuffer[CH_BUFFER_INPUT_DATA+0],
 			2);
-		if (address < CH_EEPROM_ADDR_RUNCODE) {
+		if (address < CH_EEPROM_ADDR_RUNCODE ||
+		    address > CH_EEPROM_ADDR_MAX) {
 			rc = CH_ERROR_INVALID_ADDRESS;
 			break;
 		}
